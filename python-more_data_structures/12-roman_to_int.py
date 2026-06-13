84d293384d30731ad2506acd8680835557b05db6
rywma <almujallireema@gmail.com>
2026-06-13 20:13:30 +0000

    Add task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    for i in range(len(roman_string)):
+        current = roman[roman_string[i]]
+        if i + 1 < len(roman_string):
+            next_val = roman[roman_string[i + 1]]
+            if current < next_val:
+                total -= current
+            else:
+                total += current
+        else:
+            total += current
+    return total
