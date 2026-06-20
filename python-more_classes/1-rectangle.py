ef6c36a44cb65842a09ce7dad20e594bf13392e3
rywma <almujallireema@gmail.com>
2026-06-20 17:40:35 +0000

    Initialize task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, 
