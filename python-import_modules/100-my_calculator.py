811f336093e80267b07e15e3bcff71858d392905
rywma <almujallireema@gmail.com>
2026-06-22 15:18:13 +0000

    Add advanced task

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
