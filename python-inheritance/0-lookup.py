b030c3e73b83d79926e8adf322224bb9caa0054a
rywma <almujallireema@gmail.com>
2026-06-27 15:52:05 +0000

    Add module docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that provides a lookup function for object attributes and methods"""
+
+
 def lookup(obj):
     """Returns list of attributes and methods of an object"""
     return dir(obj)
