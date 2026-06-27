e1171bf38de61c2ace450365f77667292629603b
rywma <almujallireema@gmail.com>
2026-06-27 18:30:24 +0000

    Fix blank space

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """A rectangle class that inherits from BaseGeometry"""
 
-
     def __init__(self, width, height):
         """Initializes Rectangle with validated width and height"""
         self.integer_validator("width", width)
