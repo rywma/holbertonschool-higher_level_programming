198cdd9de4497d4f2e477033744065b941e0e479
rywma <almujallireema@gmail.com>
2026-07-04 16:20:06 +0000

    Fix typo

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Write a string to a text file (UTF8) and return chars written."""
     with open(filename, mode="w", encoding="utf-8") as f:
-        return f.write(txt)
+        return f.write(text)
