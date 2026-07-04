d829de1cfb4ee03a9eea424127f6bbacad2a8149
rywma <almujallireema@gmail.com>
2026-07-04 16:18:24 +0000

    Add task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8) and return chars written."""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(txt)
