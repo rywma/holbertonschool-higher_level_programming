3bb59482338352c59d5cc005195f28ef8764e2a8
rywma <almujallireema@gmail.com>
2026-07-04 17:37:35 +0000

    Add task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that converts a json string to a python data structure."""
+
+
+def from_json_string(my_str):
+    """Return a python object represented by a json string."""
+    return json.loads(my_str)
