204fa9efe3ca40dd7c07e01f68342b6e531643dc
rywma <almujallireema@gmail.com>
2026-07-25 18:56:06 +0000

    Add task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates user_0d_1 with all privileges, password user_0d_1_pwd, if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
